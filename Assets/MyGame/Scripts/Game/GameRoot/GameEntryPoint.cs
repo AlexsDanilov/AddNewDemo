95a2595ca1a914c0552c47d7b8ff505358f897b4
AlexsDanilov <ohawe1984@gmail.com>
2024-08-09 17:53:56 +0300

    Пятничная доабвка , точка входа 1.

--- Assets/MyGame/Scripts/Game/GameRoot/GameEntryPoint.cs
+++ Assets/MyGame/Scripts/Game/GameRoot/GameEntryPoint.cs
@@ -1,5 +1,9 @@
+using myGame.Scripts;
+using myGame.Scripts.Game.Gameplay.Root;
 using MyGame.Scripts.Utils;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MyGame.Scripts.Game.GameRoot
 {
@@ -26,7 +30,7 @@ namespace MyGame.Scripts.Game.GameRoot
             _coroutines = new GameObject("[COROUTINES]").AddComponent<Coroutines>();
             Object.DontDestroyOnLoad( _coroutines.gameObject );
 
-            var prefabUIRoot = Resources.Load<UIRootView>("[UIRoot]");
+            var prefabUIRoot = Resources.Load<UIRootView>("UIRoot");
             _uiRoot = Object.Instantiate(prefabUIRoot);
             Object.DontDestroyOnLoad(_uiRoot.gameObject);
 
@@ -34,9 +38,49 @@ namespace MyGame.Scripts.Game.GameRoot
 
         private void RunGame()
         {
+#if UNITY_EDITOR
+            var sceneName = SceneManager.GetActiveScene().name;
 
+            if (sceneName == Scenes.GAMEPLAY)
+            {
+                // var enterParams = new GameplayEnterParams("ddd.save", 1);
+                // _coroutines.StartCoroutine(LoadAndStartGameplay(enterParams));
+                _coroutines.StartCoroutine(LoadAndStartGameplay());
+                return;
+            }
+
+           // if (sceneName == Scenes.MAIN_MENU)
+            //{
+            //    _coroutines.StartCoroutine(LoadAndStartMainMenu());
+            //}
+
+            if (sceneName != Scenes.BOOT)
+            {
+                return;
+            }
+#endif
+            _coroutines.StartCoroutine(LoadAndStartGameplay());
         }
 
+        private IEnumerator LoadAndStartGameplay()
+        {
+            _uiRoot.ShowLoadingScreen();
+            yield return LoadScene(Scenes.BOOT);
+            yield return LoadScene(Scenes.GAMEPLAY);
+
+            yield return new WaitForSeconds(1);
+
+            var sceneEntryPoint = Object.FindAnyObjectByType<GameplayEntryPoint>();
+            sceneEntryPoint.Run();
+
+            
+            _uiRoot.HideLoadingScreen();
+        }
+
+        private IEnumerator LoadScene(string sceneName)
+        {
+            yield return SceneManager.LoadSceneAsync(sceneName);
+        }
     }
 
 
---
+++ Assets/MyGame/Scripts/Game/Gameplay/Root/GameplayEntryPoint.cs
@@ -0,0 +1,46 @@
+﻿//using myGame.Scripts.Game.Gameplay.Root.View;
+//using myGame.Scripts.Game.MainMenu.Root;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace myGame.Scripts.Game.Gameplay.Root
+{
+    public class GameplayEntryPoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject _sceneRootBinder;
+
+
+        public void Run()
+        {
+            Debug.Log("Gameplay scene loaded");
+        }
+
+        /*public Observable<GameplayExitParams> Run(DIContainer gameplayContainer, GameplayEnterParams enterParams)
+        {
+            GameplayRegistrations.Register(gameplayContainer, enterParams);
+            var gameplayViewModelsContainer = new DIContainer(gameplayContainer);
+            GameplayViewModelsRegistrations.Register(gameplayViewModelsContainer);
+            
+            ///
+            
+            // Для теста:
+            gameplayViewModelsContainer.Resolve<UIGameplayRootViewModel>();
+            gameplayViewModelsContainer.Resolve<WorldGameplayRootViewModel>();
+            
+            var uiRoot = gameplayContainer.Resolve<UIRootView>();
+            var uiScene = Instantiate(_sceneUIRootPrefab);
+            uiRoot.AttachSceneUI(uiScene.gameObject);
+
+            var exitSceneSignalSubj = new Subject<Unit>();
+            uiScene.Bind(exitSceneSignalSubj);
+
+            Debug.Log($"GAMEPLAY ENTRY POINT: save file name = {enterParams.SaveFileName}, level to load = {enterParams.LevelNumber}");
+            
+            var mainMenuEnterParams = new MainMenuEnterParams("Fatality");
+            var exitParams = new GameplayExitParams(mainMenuEnterParams);
+            var exitToMainMenuSceneSignal = exitSceneSignalSubj.Select(_ => exitParams);
+
+            return exitToMainMenuSceneSignal;
+        }*/
+    }
+}
